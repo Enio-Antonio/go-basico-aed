4d06541d5dede94c2305af6e4e03d35bedc6fd45
Enio-Antonio <enioaclizieri@gmail.com>
2025-03-14 02:37:56 -0300

    Atualiza arrays.go

--- arrays.go
+++ arrays.go
@@ -5,6 +5,12 @@ import "fmt"
 func main() {
 	var vetor = [2]int {1, 2}
 
+	// Outra forma de inicializar
+	var vetor_2 [2]int 
+	for i := 0; i < 2; i++ {
+		vetor_2[i] = i
+	}
+
 	// Percorrendo normal
 	for i := 0; i < len(vetor); i++ {
 		fmt.Println(vetor[i])
@@ -14,5 +20,8 @@ func main() {
 	for _, elemento := range vetor { // Não for usar o índice, o nome deve ser '_'
 		fmt.Println(elemento)
 	}
+
+	// Ignorar (se não acontece esse de compilação)
+	fmt.Println(vetor_2[0])
 	
 }
