95fd8057cda3871bdd3d4e99a76289190f87f067
Enio-Antonio <enioaclizieri@gmail.com>
2025-07-08 01:31:21 -0300

    Correção em arvore.go

--- Algoritmos/Árvore de busca binária/arvore.go
+++ Algoritmos/Árvore de busca binária/arvore.go
@@ -1,35 +1,21 @@
-// func levelOrderNav()
-// func Remove(val int)
-// func size() int
-// func height() int
-// func preOrderNav()
-// func inOrderNav()
-// func postOrderNav()
-
-// pseudocódigo para navegação
-// a fila é criada com o nó raiz
-//enquanto fila != vazia
-//	frente = fila.dequeue()
-//	print(frent.val)
-//	se frente.esq != nil {fila.enqueue(frente.esq)}
-//	se frente.dir != nil {fila.enqueue(frente.dir)}
-
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-type BTSNode struct {
-	left *BTSNode
-	val int
-	right *BTSNode
+type BSTNode struct {
+	left  *BSTNode
+	val   int
+	right *BSTNode
 }
 
-func createNode(val int) *BTSNode {
-	return &BTSNode{val: val}
+func createNode(val int) *BSTNode {
+	return &BSTNode{val: val}
 }
 
-func (node *BTSNode) add(val int) {
+func (node *BSTNode) add(val int) {
 	if node != nil {
 		if val <= node.val {
 			if node.left == nil {
@@ -47,7 +33,7 @@ func (node *BTSNode) add(val int) {
 	}
 }
 
-func (node *BTSNode) search(val int) bool {
+func (node *BSTNode) search(val int) bool {
 	if node == nil {
 		return false
 	} else {
@@ -61,60 +47,107 @@ func (node *BTSNode) search(val int) bool {
 	}
 }
 
-func (node *BTSNode) min() int {
-	if node == nil { return -1 }
+func (node *BSTNode) min() int {
+	if node == nil {
+		return -1
+	}
 	if node.left == nil {
 		return node.val
 	} else {
 		return node.left.min()
 	}
 }
-
-func (node *BTSNode) max() int {
-	if node == nil { return -1 }
+func (node *BSTNode) max() int {
+	if node == nil {
+		return -1
+	}
 	if node.right == nil {
 		return node.val
 	} else {
 		return node.right.max()
 	}
 }
-
 func (node *BSTNode) height() int {
+	if node == nil {
+		return -1
+	}
 	hl := node.left.height()
 	hr := node.right.height()
-	return Math.Max(hl, hr)
+	return 1 + int(math.Max(float64(hl), float64(hr)))
 }
- 
-func (node *BSTNode) preOrder() {
+func (node *BSTNode) printPre() {
 	if node != nil {
 		fmt.Print(node.val, ", ")
-		node.left.preOrder()
-		node.right.preOrder()
+		node.left.printPre()
+		node.right.printPre()
+	}
+}
+func (node *BSTNode) PrintIn() {
+	if node == nil {
+		return
+	}
+	node.left.PrintIn()
+	fmt.Print(node.val, " , ")
+	node.right.PrintIn()
+}
+func (node *BSTNode) PrintPost() {
+	if node == nil {
+		return
+	}
+	node.left.PrintPost()
+	node.right.PrintPost()
+	fmt.Print(node.val, ", ")
+}
+
+func (node *BSTNode) PrintLevels() {
+	if node == nil {
+		return
+	}
+	// Supondo uma fila do tipo BSTNode implementada
+	queue := Queue{}
+	queue.Enqueue(node)
+
+	for !queue.isEmpty() {
+		current := queue.Dequeue()
+
+		fmt.Print(current.val, ", ")
+
+		if current.left != nil {
+			queue.Enqueue(current.left)
+		}
+		if current.right != nil {
+			queue.Enqueue(current.right)
+		}
 	}
 }
 
-func (node *BTSNode) remove(val int) *BTSNode {
-	if node == nil {return nil}
-	if val < node.val { node.left = node.left.remove(val)}
-	else if val > nodel.val { node.right = node.right.remove(val)}
-	else { 
+func (node *BSTNode) remove(val int) *BSTNode {
+	if node == nil {
+		return nil
+	}
+	if val < node.val {
+		node.left = node.left.remove(val)
+	} else if val > node.val {
+		node.right = node.right.remove(val)
+	} else {
 		if node.left == nil && node.right == nil {
 			return nil
 		} else if node.left != nil && node.right == nil {
-			return node.left 
+			return node.left
 		} else if node.left == nil && node.right != nil {
 			return node.right
-		} else {	
+		} else {
 			max := node.left.max()
 			node.val = max
 			node.left = node.left.remove(max)
 		}
-		return node
 	}
+	return node
 }
 
 func main() {
-	var bt BTSNode
-	bt.add(1)
-	fmt.Println(bt.search(1))
+	root := BSTNode{val: 5}
+	root.add(7)
+	root.add(3)
+	root.printPre()
 }
--- Algoritmos/Árvore de busca binária/arvoreit.go
+++ Algoritmos/Árvore de busca binária/arvoreit.go
@@ -1,82 +1,139 @@
-// func levelOrderNav()
-// func Remove(val int)
-// func size() int
-// func height() int
-// func preOrderNav()
-// func inOrderNav()
-// func postOrderNav()
-
-package main
-
-import "fmt"
-
-type BSNode struct {
-	val int
-	dir *BSNode
-	esq *BSNode
+type BSTNode struct {
+	left  *BSTNode
+	val   int
+	right *BSTNode
 }
 
-type BinaryTree struct {
-	root *BSNode
+func createNode(val int) *BSTNode {
+	return &BSTNode{val: val}
 }
 
-func (bs *BinaryTree) add(e int) {
-	newn := BSNode{val: e, dir: nil, esq: nil}
-	if bs.root == nil {
-		bs.root = &newn
-
-	} else {
-		aux := bs.root
-		var p *BSNode
-		for aux != nil {
-			p = aux
-			if e > aux.val {
-				aux = aux.dir
-			} else if e < aux.val {
-				aux = aux.esq
+func (node *BSTNode) add(val int) {
+	if node != nil {
+		if val <= node.val {
+			if node.left == nil {
+				node.left = createNode(val)
+			} else {
+				node.left.add(val)
+			}
+		} else {
+			if node.right == nil {
+				node.right = createNode(val)
+			} else {
+				node.right.add(val)
 			}
 		}
+	}
+}
 
-		if e > p.val {
-			p.dir = &newn
-		} else if e < p.val {
-			p.esq = &newn
+func (node *BSTNode) search(val int) bool {
+	if node == nil {
+		return false
+	} else {
+		if val < node.val {
+			return node.left.search(val)
+		} else if val > node.val {
+			return node.right.search(val)
+		} else {
+			return true
 		}
- 
 	}
 }
 
-func (bs *BinaryTree) min() int {
-	aux := bs.root
-	var p *BSNode
-	for aux != nil {
-		p = aux
-		//fmt.Println(aux.val)
-		aux = aux.esq
+func (node *BSTNode) min() int {
+	if node == nil {
+		return -1
 	}
-	return p.val
-}
-
-func (bs *BinaryTree) max() int {
-	aux := bs.root
-	var p *BSNode
-	for aux != nil {
-		p = aux
-		aux = aux.dir
+	if node.left == nil {
+		return node.val
+	} else {
+		return node.left.min()
 	}
-	return p.val
+}
+func (node *BSTNode) max() int {
+	if node == nil {
+		return -1
+	}
+	if node.right == nil {
+		return node.val
+	} else {
+		return node.right.max()
+	}
+}
+func (node *BSTNode) height() int {
+	if node == nil {
+		return -1
+	}
+	hl := node.left.height()
+	hr := node.right.height()
+	return 1 + int(math.Max(float64(hl), float64(hr)))
+}
+func (node *BSTNode) printPre() {
+	if node != nil {
+		fmt.Print(node.val, ", ")
+		node.left.printPre()
+		node.right.printPre()
+	}
+}
+func (node *BSTNode) PrintIn() {
+	if node == nil {
+		return
+	}
+	node.left.PrintIn()
+	fmt.Print(node.val, " , ")
+	node.right.PrintIn()
+}
+func (node *BSTNode) PrintPost() {
+	if node == nil {
+		return
+	}
+	node.left.PrintPost()
+	node.right.PrintPost()
+	fmt.Print(node.val, ", ")
 }
 
-//func (bs *BinaryTree) search(e int) bool {
+func (node *BSTNode) PrintLevels() {
+	if node == nil {
+		return
+	}
+	// Supondo uma fila do tipo BSTNode implementada
+	queue := Queue{}
+	queue.Enqueue(node)
 
-func main() {
-	var bs BinaryTree
-	bs.add(5)
-	bs.add(4)
-	bs.add(1)
-	a := bs.min()
-	m := bs.max()
-	fmt.Println(a)
-	fmt.Println(m)
-	fmt.Println("Acabou")
+	for !queue.isEmpty() {
+		current := queue.Dequeue()
+
+		fmt.Print(current.val, ", ")
+
+		if current.left != nil {
+			queue.Enqueue(current.left)
+		}
+		if current.right != nil {
+			queue.Enqueue(current.right)
+		}
+	}
 }
+
+func (node *BSTNode) remove(val int) *BSTNode {
+	if node == nil {
+		return nil
+	}
+	if val < node.val {
+		node.left = node.left.remove(val)
+	} else if val > node.val {
+		node.right = node.right.remove(val)
+	} else {
+		if node.left == nil && node.right == nil {
+			return nil
+		} else if node.left != nil && node.right == nil {
+			return node.left
+		} else if node.left == nil && node.right != nil {
+			return node.right
+		} else {
+			max := node.left.max()
+			node.val = max
+			node.left = node.left.remove(max)
+		}
+	}
+	return node
+} 
